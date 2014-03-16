8341cdec7e58a18b55e6ba4360810dfa04870b1f
Fran Hermoso <franhp@gmail.com>
2014-03-16 17:45:10 +0000

    better url handling for nginx

--- homeServer/settings.py
+++ homeServer/settings.py
@@ -80,8 +80,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, '../thumbnails'),
-    os.path.join(SITE_ROOT, '../videos'),
+    ('thumbnails', os.path.join(SITE_ROOT, '../thumbnails')),
+    ('videos', os.path.join(SITE_ROOT, '../videos')),
 )
 
 # List of finder classes that know how to find static files in
